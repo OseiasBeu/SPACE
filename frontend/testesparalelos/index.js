2fb5daf3b460ed9614a0246b85be53dbc1b55574
Oseias Beu <oseiasbeu@outlook.com>
2018-04-01 02:27:02 -0300

    Site em 66% - Terminando a página sobre nós - em 50%

--- frontend/testesparalelos/index.js
+++ frontend/testesparalelos/index.js
@@ -1,23 +1,31 @@
-let imgIco = document.querySelector('img');
+let imgIco = document.querySelector('#imgIC');
+let missao = document.querySelector('.missaoCapa');
+let valores = document.querySelector('.valoresCapa');
+let visao = document.querySelector('.visaoCapa');
 
-function girarD() {
+function girarMissao() {
+    imgIco.style.transform = 'rotate(120deg)';
+    imgIco.style.transition = '2s';
+    missao.style.color = 'whitesmoke';
+    missao.style.transition = '2s';
+    visao.style.color = 'rgba(0, 0, 0, 0)';
+    valores.style.color = 'rgba(0, 0, 0, 0)';
+}
 
+function girarVisao() {
+    imgIco.style.transform = 'rotate(240deg)';
+    imgIco.style.transition = '2s';
+    visao.style.color = 'whitesmoke';
+    visao.style.transition = '2s';
+    valores.style.color = 'rgba(0, 0, 0, 0)';
+    missao.style.color = 'rgba(0, 0, 0, 0)';
+}
+
+function girarValores() {
     imgIco.style.transform = 'rotate(360deg)';
     imgIco.style.transition = '2s';
-    dec;
-}
-
-
-function girarE() {
-    // imgIco.style.transform = 'scale(0.5,0.5)';
-    imgIco.style.transform = 'rotate(0deg)';
-    imgIco.style.transition = '2s';
-    dec;
-}
-
-let dec = {
-    method: function decrece() {
-        return imgIco.style.transform = 'scale(0.5,0.5)';
-    }
-
+    valores.style.color = 'whitesmoke';
+    valores.style.transition = '2s';
+    missao.style.color = 'rgba(0, 0, 0, 0)';
+    visao.style.color = 'rgba(0, 0, 0, 0)';
 }
