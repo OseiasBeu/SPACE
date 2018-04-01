08445a8f39029761934b9e9bbbdc1f87b54c1c19
Oseias Beu <oseiasbeu@outlook.com>
2018-04-01 15:36:52 -0300

    Site em 66% - Páginas principais terminadas

--- frontend/scripts/about.js
+++ frontend/scripts/about.js
@@ -5,29 +5,29 @@ let visao = document.querySelector('.visaoCapa');
 
 function girarMissao() {
     imgIco.style.transform = 'rotate(120deg)';
-    imgIco.style.transition = '2s';
+    imgIco.style.transition = '1.2s';
     missao.style.color = 'whitesmoke';
     missao.style.border = '1px solid rgb(68, 66, 66);'
-    missao.style.transition = '2s';
+    missao.style.transition = '1.2s';
     visao.style.color = 'rgba(0, 0, 0, 0)';
     valores.style.color = 'rgba(0, 0, 0, 0)';
 }
 
 function girarVisao() {
     imgIco.style.transform = 'rotate(240deg)';
-    imgIco.style.transition = '2s';
+    imgIco.style.transition = '1.2s';
     visao.style.color = 'whitesmoke';
     visao.style.border = '1px solid rgb(68, 66, 66);'
-    visao.style.transition = '2s';
+    visao.style.transition = '1.2s';
     valores.style.color = 'rgba(0, 0, 0, 0)';
     missao.style.color = 'rgba(0, 0, 0, 0)';
 }
 
 function girarValores() {
     imgIco.style.transform = 'rotate(360deg)';
-    imgIco.style.transition = '2s';
+    imgIco.style.transition = '1.2s';
     valores.style.color = 'whitesmoke';
-    valores.style.transition = '2s';
+    valores.style.transition = '1.2s';
     valores.style.border = '1px solid rgb(68, 66, 66);'
     missao.style.color = 'rgba(0, 0, 0, 0)';
     visao.style.color = 'rgba(0, 0, 0, 0)';
--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -3,14 +3,14 @@ let slogan = document.querySelector('.slogan');
 
 function aum() {
     imgIco.style.transform = 'scale(0.7,0.7)';
-    imgIco.style.transition = '1.2s';
+    imgIco.style.transition = '0.8s';
     slogan.style.transform = 'scale(1.2,1.2)';
-    slogan.style.transition = '1.2s';
+    slogan.style.transition = '0.8s';
 }
 
 function dim() {
     imgIco.style.transform = 'scale(1,1)';
-    imgIco.style.transition = '1.2s';
+    imgIco.style.transition = '0.8s';
     slogan.style.transform = 'scale(1,1)';
-    slogan.style.transition = '1.2s';
+    slogan.style.transition = '0.8s';
 }
